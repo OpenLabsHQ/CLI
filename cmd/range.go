9427025b3dabdc7c5bbf44247045907f4f2f8fda
Adamkadaban <adamkadaban@gmail.com>
2025-02-13 10:47:40 -0500

    add range deploy command

--- cmd/range.go
+++ cmd/range.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
     "fmt"
+    "net/http"
+    "encoding/json"
+    "bytes"
+
     "github.com/spf13/cobra"
 )
 
@@ -9,14 +13,54 @@ var rangeCmd = &cobra.Command{
     Use:   "range",
     Short: "Deploy, and manage range",
     Long:  "This command will let you deploy, power on/off, and manage your range.",
+}
+
+var deployRangeCmd = &cobra.Command{
+    Use:   "deploy",
+    Short: "Deploy a range",
+    Long:  "This command will deploy a range to the OpenLabs API.",
     Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Placeholder")
-        //client := api.Client{BaseURL: "https://localhost/api/v1/templates"}
-        //data, err := client.
+        if len(args) == 0 {
+            fmt.Println("No range ID provided")
+            return
+        }
+
+        err := deployRange(args)
+        if err != nil {
+            fmt.Println(err)
+        }
     },
 }
 
+func deployRange(rangeIDs []string) error {
+    url := "http://localhost:8000/api/v1/ranges/deploy"
+
+
+    requestBody, err := json.Marshal(map[string]interface{}{
+        "range_ids": rangeIDs,
+    })
+    if err != nil {
+        return fmt.Errorf("Failed to marshal request body: %s", err)
+    }
+
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+    if err != nil {
+        return fmt.Errorf("Failed to create request: %s", err)
+    }
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return fmt.Errorf("Failed to send request: %s", err)
+    }
+    defer resp.Body.Close()
+    
+    return nil
+}
+
 func init() {
+    rangeCmd.AddCommand(deployRangeCmd)
     rootCmd.AddCommand(rangeCmd)
 }
 
